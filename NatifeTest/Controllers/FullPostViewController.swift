2296b514d85fd77780e71795e94b5e4efa98dc2d
Michael <miha7mp@gmail.com>
2022-03-26 16:37:41 +0200

    Implement full post controller

--- NatifeTest/Controllers/FullPostViewController.swift
+++ NatifeTest/Controllers/FullPostViewController.swift
@@ -16,8 +16,63 @@ class FullPostViewController: UIViewController {
     @IBOutlet weak var likesLabel: UILabel!
     @IBOutlet weak var timeLabel: UILabel!
     
+    // MARK: - Constants
+    let imageService = ImageService()
+    let dateService = DateService()
+    
+    // MARK: - Variables
+    var post: FullPost!
+    
     // MARK: - Lifecycle methods
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        initUiFromPost()
+    }
+    
+    private func initUiFromPost() {
+        URLCache.shared.removeAllCachedResponses()
+        
+        title = post.title
+        titleLabel.text = post.title
+        textLabel.text = post.text
+        likesLabel.text = "\(post.likesCount)"
+        
+        let diff = dateService.getDifferenceBetween(unixEpoch: post.timeshamp, and: Date())
+        if let diffString = dateService.getDateStringFrom(timeInterval: diff) {
+            timeLabel.text = "\(diffString) назад"
+        } else {
+            timeLabel.text = ""
+        }
+        
+        fetchAndSetImagesFromPost()
+    }
+    
+    private func fetchAndSetImagesFromPost() {
+        for imageUrl in post.images {
+            let view = UIActivityIndicatorView()
+            view.startAnimating()
+            imagesStackView.addArrangedSubview(view)
+            
+            imageService.fetchImage(from: imageUrl) { image in
+                if let image = image {
+                    let imageView = self.getImageViewFor(image: image)
+                    self.imagesStackView.addArrangedSubview(imageView)
+                } else {
+                    let imageView = self.getImageViewFor(image: UIImage(named: "noImage.svg")!)
+                    self.imagesStackView.addArrangedSubview(imageView)
+                }
+                self.imagesStackView.removeArrangedSubview(view)
+                view.removeFromSuperview()
+            }
+        }
+    }
+    
+    private func getImageViewFor(image: UIImage) -> UIImageView {
+        let imageView = UIImageView(image: image)
+        imageView.contentMode = .scaleAspectFit
+        imageView.constraintAspectRatioToImage()
+        
+        return imageView
     }
 }
