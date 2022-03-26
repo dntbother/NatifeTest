059a398f1230c67996c8499182762e0f54389fc1
Michael <miha7mp@gmail.com>
2022-03-26 16:35:55 +0200

    Add cell expantion protocol, date extension

---
+++ NatifeTest/Protocols.swift
@@ -0,0 +1,12 @@
+//
+//  Protocols.swift
+//  NatifeTest
+//
+//  Created by Mykhailo Petukhov on 25.03.2022.
+//
+
+import Foundation
+
+protocol CellExpantionDelegate {
+    func cellExpanded(_ postId: Int)
+}
